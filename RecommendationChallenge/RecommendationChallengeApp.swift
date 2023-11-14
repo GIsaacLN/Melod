5a90ca2b2a6bbc9ead48f68cc9d03d403cfb7570
Isaac Lopez <isaac.lopez.1b@gmail.com>
2023-11-14 20:32:22 +0100

    Initial Commit

---
+++ RecommendationChallenge/RecommendationChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecommendationChallengeApp.swift
+//  RecommendationChallenge
+//
+//  Created by Gustavo Isaac Lopez Nunez on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RecommendationChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
